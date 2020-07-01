b03b4e3bf616b010f704d5a114ba60eadf7d9028
jmorales113 <jmorales113@gmail.com>
2020-07-01 12:58:14 -0700

    added background img

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 // add Event Listener on widow on keydown event
-// create constant variable to queryselect audio data-key = e.keyCode
+// create constant variable to queryselecter audio data-key = `audio[data-key={"e.keyCode}"]`
+// console.log(e)
+// create constant to queryselect class key
+// console.log(key)
+// if no audio then return  //Stops the function from running all together
+// audio.currentTime = 0 // rewind to the start
+// audio.play() // plays the audio
+// add class of playing to key variable
+
+// Need to take away border after click
+// create constant varaible to queryselectAll key class
+// console.log(keys)
+// loop through keys with forEach, addeventlistner(transitionend), second parameter/function removeTransition
+// Create function removeTransition
+// console.log(e)
+// if property name does not equal "transform" return noting // This will skip if it's not a transform
+//console.log(e.propertyName)
+//console.log(this)
+// use this to remove class playing
