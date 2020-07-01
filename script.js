b5442e3cca37abaeaf4082c30269ced9d8dbedb2
jmorales113 <jmorales113@gmail.com>
2020-07-01 13:24:58 -0700

    added keydown event listener

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-// add Event Listener on widow on keydown event
-// create constant variable to queryselecter audio data-key = `audio[data-key={"e.keyCode}"]`
-// console.log(e)
-// create constant to queryselect class key
-// console.log(key)
-// if no audio then return  //Stops the function from running all together
-// audio.currentTime = 0 // rewind to the start
-// audio.play() // plays the audio
-// add class of playing to key variable
+// add Event Listener on window on keydown event
+window.addEventListener("keydown", (e) => {
+  // create constant variable to queryselecter audio data-key = `audio[data-key={"e.keyCode}"]`
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  // create constant to queryselect class key
+  // console.log(key)
+  // if no audio then return  //Stops the function from running all together
+  // audio.currentTime = 0 // rewind to the start
+  // audio.play() // plays the audio
+  // add class of playing tao key variable
+});
 
 // Need to take away border after click
 // create constant varaible to queryselectAll key class
